b3307124ba61a9f4c989890418ecde280e100bb5
Robert Smith <r2theob23@gmail.com>
2012-07-19 22:14:25 -0400

    Added placeholders and modified main.js file

--- week3/Good_App/js/main.js
+++ week3/Good_App/js/main.js
@@ -1,12 +1,19 @@
 //Project 2
 //MiU 1207
 //Robert Smith
- 
+ var parseAssignmentForm = function(data){
+ 	//uses form data here:
+ };
  $(document).bind('pageinit', function(){
 
  	var addItem = $('#addassignmentform');
 
- 	addItem.validate();
+ 	addItem.validate({
+ 		submitHandler: function(){
+ 			var data = addItem.serialize();
+ 			parseAssignmentForm(data);
+ 		}
+ 	});
 
 
  });
