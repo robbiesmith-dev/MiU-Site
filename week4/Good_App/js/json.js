c049f82f0f9e192311dce798dd0779f56041f4cb
Robert Smith <r2theob23@gmail.com>
2012-07-26 19:19:45 -0400

    Added week three to week four folder to begin project

---
+++ week4/Good_App/js/json.js
@@ -0,0 +1,227 @@
+//Robert Smith
+//MiU 1207
+//Project 2 json
+
+var json = {
+	"assignment1": {
+		"GorI": ["Group or Individual: ", "Individual"],
+		"clname": ["Class Name: ", "VFW"],
+		"asname": ["Assignment Name: ", "Project 4"],
+		"dudate": ["Due Date: ", "2012-06-21"],
+		"prior": ["Priority: ", "100"],
+		"InsName": ["Instructor Name: ", "Chad Gibson"],
+		"email": ["Instructor E-mail: ", "Cgibson@me.com"],
+		"reminder": ["Set Reminders: ", "Two Days Prior"],
+		"notes": ["Notes: ", "Final Assignment for VFW."]
+	},
+	"assignment2": {
+		"GorI": ["Group or Individual: ", "Individual"],
+		"clname": ["Class Name: ", "MIU"],
+		"asname": ["Assignment Name: ", "Project 1"],
+		"dudate": ["Due Date: ", "2012-07-02"],
+		"prior": ["Priority: ", "100"],
+		"InsName": ["Instructor Name: ", "Robin"],
+		"email": ["Instructor E-mail: ", "robin@me.com"],
+		"reminder": ["Set Reminders: ", "One Day Prior"],
+		"notes": ["Notes: ", "First Project for MIU."]
+	},
+	"assignment3": {
+		"GorI": ["Group or Individual: ", "Individual"],
+		"clname": ["Class Name: ", "MIU"],
+		"asname": ["Assignment Name: ", "Project 2"],
+		"dudate": ["Due Date: ", "2012-07-09"],
+		"prior": ["Priority: ", "75"],
+		"InsName": ["Instructor Name: ", "Robin"],
+		"email": ["Instructor E-mail: ", "robin@me.com"],
+		"reminder": ["Set Reminders: ", "Two Days Prior"],
+		"notes": ["Notes: ", "Second Project for MIU."]
+	},
+	"assignment4": {
+		"GorI": ["Group or Individual: ", "Individual"],
+		"clname": ["Class Name: ", "MIU"],
+		"asname": ["Assignment Name: ", "Project 3"],
+		"dudate": ["Due Date: ", "2012-07-15"],
+		"prior": ["Priority: ", "80"],
+		"InsName": ["Instructor Name: ", "Robin"],
+		"email": ["Instructor E-mail: ", "robin@me.com"],
+		"reminder": ["Set Reminders: ", "One Day Prior"],
+		"notes": ["Notes: ", "Third Project for MIU."]
+	},
+	"assignment5": {
+		"GorI": ["Group or Individual: ", "Individual"],
+		"clname": ["Class Name: ", "MIU"],
+		"asname": ["Assignment Name: ", "Project 4"],
+		"dudate": ["Due Date: ", "2012-07-22"],
+		"prior": ["Priority: ", "90"],
+		"InsName": ["Instructor Name: ", "Robin"],
+		"email": ["Instructor E-mail: ", "robin@me.com"],
+		"reminder": ["Set Reminders: ", "One Day Prior"],
+		"notes": ["Notes: ", "Forth Project for MIU."]
+	},
+	"assignment6": {
+		"GorI": ["Group or Individual: ", "Individual"],
+		"clname": ["Class Name: ", "MIU"],
+		"asname": ["Assignment Name: ", "Disscusion 1"],
+		"dudate": ["Due Date: ", "2012-07-03"],
+		"prior": ["Priority: ", "50"],
+		"InsName": ["Instructor Name: ", "Robin"],
+		"email": ["Instructor E-mail: ", "robin@me.com"],
+		"reminder": ["Set Reminders: ", "Two Day Prior"],
+		"notes": ["Notes: ", "First Disscusion Post for MIU."]
+	},
+	"assignment7": {
+		"GorI": ["Group or Individual: ", "Individual"],
+		"clname": ["Class Name: ", "MIU"],
+		"asname": ["Assignment Name: ", "Disscusion 2"],
+		"dudate": ["Due Date: ", "2012-07-10"],
+		"prior": ["Priority: ", "60"],
+		"InsName": ["Instructor Name: ", "Robin"],
+		"email": ["Instructor E-mail: ", "robin@me.com"],
+		"reminder": ["Set Reminders: ", "One Day Prior"],
+		"notes": ["Notes: ", "Second Disscusion Post for MIU."]
+	},
+	"assignment8": {
+		"GorI": ["Group or Individual: ", "Individual"],
+		"clname": ["Class Name: ", "MIU"],
+		"asname": ["Assignment Name: ", "Disscusion 3"],
+		"dudate": ["Due Date: ", "2012-07-17"],
+		"prior": ["Priority: ", "80"],
+		"InsName": ["Instructor Name: ", "Robin"],
+		"email": ["Instructor E-mail: ", "robin@me.com"],
+		"reminder": ["Set Reminders: ", "One Day Prior"],
+		"notes": ["Notes: ", "Third Disscusion Post for MIU."]
+	},
+	"assignment9": {
+		"GorI": ["Group or Individual: ", "Individual"],
+		"clname": ["Class Name: ", "MIU"],
+		"asname": ["Assignment Name: ", "Disscusion 4"],
+		"dudate": ["Due Date: ", "2012-07-24"],
+		"prior": ["Priority: ", "90"],
+		"InsName": ["Instructor Name: ", "Robin"],
+		"email": ["Instructor E-mail: ", "robin@me.com"],
+		"reminder": ["Set Reminders: ", "One Day Prior"],
+		"notes": ["Notes: ", "Final Disscusion Post for MIU."]
+	},
+	"assignment10": {
+		"GorI": ["Group or Individual: ", "Individual"],
+		"clname": ["Class Name: ", "MIU"],
+		"asname": ["Assignment Name: ", "Video 1"],
+		"dudate": ["Due Date: ", "2012-07-03"],
+		"prior": ["Priority: ", "100"],
+		"InsName": ["Instructor Name: ", "Robin"],
+		"email": ["Instructor E-mail: ", "robin@me.com"],
+		"reminder": ["Set Reminders: ", "One Day Prior"],
+		"notes": ["Notes: ", "First Video Post for MIU."]
+	},
+	"assignment11": {
+		"GorI": ["Group or Individual: ", "Individual"],
+		"clname": ["Class Name: ", "MIU"],
+		"asname": ["Assignment Name: ", "Video 2"],
+		"dudate": ["Due Date: ", "2012-07-10"],
+		"prior": ["Priority: ", "100"],
+		"InsName": ["Instructor Name: ", "Robin"],
+		"email": ["Instructor E-mail: ", "robin@me.com"],
+		"reminder": ["Set Reminders: ", "One Day Prior"],
+		"notes": ["Notes: ", "Second Video Post for MIU."]
+	},
+	"assignment12": {
+		"GorI": ["Group or Individual: ", "Individual"],
+		"clname": ["Class Name: ", "MIU"],
+		"asname": ["Assignment Name: ", "Video 3"],
+		"dudate": ["Due Date: ", "2012-07-17"],
+		"prior": ["Priority: ", "100"],
+		"InsName": ["Instructor Name: ", "Robin"],
+		"email": ["Instructor E-mail: ", "robin@me.com"],
+		"reminder": ["Set Reminders: ", "One Day Prior"],
+		"notes": ["Notes: ", "Third Video Post for MIU."]
+	},
+	"assignment13": {
+		"GorI": ["Group or Individual: ", "Individual"],
+		"clname": ["Class Name: ", "MIU"],
+		"asname": ["Assignment Name: ", "Video 4"],
+		"dudate": ["Due Date: ", "2012-07-23"],
+		"prior": ["Priority: ", "100"],
+		"InsName": ["Instructor Name: ", "Robin"],
+		"email": ["Instructor E-mail: ", "robin@me.com"],
+		"reminder": ["Set Reminders: ", "One Day Prior"],
+		"notes": ["Notes: ", "Final Video Post for MIU."]
+	},
+	"assignment14": {
+		"GorI": ["Group or Individual: ", "Individual"],
+		"clname": ["Class Name: ", "Statistics"],
+		"asname": ["Assignment Name: ", "Test 1"],
+		"dudate": ["Due Date: ", "2012-08-01"],
+		"prior": ["Priority: ", "100"],
+		"InsName": ["Instructor Name: ", "Robert Hoffman"],
+		"email": ["Instructor E-mail: ", "Rhoffman@me.com"],
+		"reminder": ["Set Reminders: ", "Two Day Prior"],
+		"notes": ["Notes: ", "First Test on Statistics."]
+	},
+	"assignment15": {
+		"GorI": ["Group or Individual: ", "Individual"],
+		"clname": ["Class Name: ", "Statistics"],
+		"asname": ["Assignment Name: ", "Test 2"],
+		"dudate": ["Due Date: ", "2012-08-07"],
+		"prior": ["Priority: ", "100"],
+		"InsName": ["Instructor Name: ", "Robert Hoffman"],
+		"email": ["Instructor E-mail: ", "Rhoffman@me.com"],
+		"reminder": ["Set Reminders: ", "Two Day Prior"],
+		"notes": ["Notes: ", "Second Test on Statistics."]
+	},
+	"assignment16": {
+		"GorI": ["Group or Individual: ", "Individual"],
+		"clname": ["Class Name: ", "Statistics"],
+		"asname": ["Assignment Name: ", "Test 3"],
+		"dudate": ["Due Date: ", "2012-08-14"],
+		"prior": ["Priority: ", "100"],
+		"InsName": ["Instructor Name: ", "Robert Hoffman"],
+		"email": ["Instructor E-mail: ", "Rhoffman@me.com"],
+		"reminder": ["Set Reminders: ", "Two Day Prior"],
+		"notes": ["Notes: ", "Third Test on Statistics."]
+	},
+	"assignment17": {
+		"GorI": ["Group or Individual: ", "Individual"],
+		"clname": ["Class Name: ", "Statistics"],
+		"asname": ["Assignment Name: ", "Test 4"],
+		"dudate": ["Due Date: ", "2012-08-21"],
+		"prior": ["Priority: ", "100"],
+		"InsName": ["Instructor Name: ", "Robert Hoffman"],
+		"email": ["Instructor E-mail: ", "Rhoffman@me.com"],
+		"reminder": ["Set Reminders: ", "Two Day Prior"],
+		"notes": ["Notes: ", "Final Test on Statistics."]
+	},
+	"assignment18": {
+		"GorI": ["Group or Individual: ", "Individual"],
+		"clname": ["Class Name: ", "Statistics"],
+		"asname": ["Assignment Name: ", "Disscusion 1"],
+		"dudate": ["Due Date: ", "2012-08-01"],
+		"prior": ["Priority: ", "100"],
+		"InsName": ["Instructor Name: ", "Robert Hoffman"],
+		"email": ["Instructor E-mail: ", "Rhoffman@me.com"],
+		"reminder": ["Set Reminders: ", "Two Day Prior"],
+		"notes": ["Notes: ", "First Disscusion on Statistics."]
+	},
+	"assignment19": {
+		"GorI": ["Group or Individual: ", "Individual"],
+		"clname": ["Class Name: ", "Statistics"],
+		"asname": ["Assignment Name: ", "Disscusion 2"],
+		"dudate": ["Due Date: ", "2012-08-07"],
+		"prior": ["Priority: ", "100"],
+		"InsName": ["Instructor Name: ", "Robert Hoffman"],
+		"email": ["Instructor E-mail: ", "Rhoffman@me.com"],
+		"reminder": ["Set Reminders: ", "Two Day Prior"],
+		"notes": ["Notes: ", "Second Disscusion on Statistics."]
+	},
+	"assignment20": {
+		"GorI": ["Group or Individual: ", "Individual"],
+		"clname": ["Class Name: ", "Statistics"],
+		"asname": ["Assignment Name: ", "Disscusion 3"],
+		"dudate": ["Due Date: ", "2012-08-14"],
+		"prior": ["Priority: ", "100"],
+		"InsName": ["Instructor Name: ", "Robert Hoffman"],
+		"email": ["Instructor E-mail: ", "Rhoffman@me.com"],
+		"reminder": ["Set Reminders: ", "Two Day Prior"],
+		"notes": ["Notes: ", "Third Disscusion on Statistics."]
+	}
+
+}
